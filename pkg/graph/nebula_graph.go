d5c37fa2c7b90091f31d61bd752ec51a94f2fda3
knwng <ken.qrose@gmail.com>
2021-12-18 11:05:23 +0800

    add lookup function to nebula

--- pkg/graph/nebula_graph.go
+++ pkg/graph/nebula_graph.go
@@ -3,24 +3,50 @@ package graph
 import (
 	"errors"
 	"fmt"
+	"strings"
+	"time"
 
+	log "github.com/sirupsen/logrus"
 	nebula "github.com/vesoft-inc/nebula-go/v2"
 )
 
-type GraphReader interface {}
+type GraphReadWriter interface {}
 
-// NebulaReader uses nebula-go sdk to connect to nebula-graphd service
-type NebulaReader struct {
-    address     string  // address of nebula-graphd
-    port        int     // port of nebula-graphd
-    username    string
-    password    string
-    graphName   string  // name of graph(or space), all the operations are applied to this graph
-    pool        *nebula.ConnectionPool
+type PrincipleNode struct {
+    Name string
+    Prop string
+    Time string
 }
 
-func NewNebulaReader(address string, port int,
-        username, password, graphName string) (*NebulaReader, error) {
+func ParsePrincipleNodes(nodeStrs []string) ([]PrincipleNode, error) {
+    nodes := make([]PrincipleNode, len(nodeStrs))
+    for i, node := range nodeStrs {
+        items := strings.Split(node, ".")
+        if len(items) != 3 {
+            return []PrincipleNode{}, errors.New(fmt.Sprintf("Principle node config format: {node_name}.{id_prop}.{time_prop}, but got: %s", node))
+        }
+        name, prop, time := items[0], items[1], items[2]
+        nodes[i] = PrincipleNode{
+            Name: name,
+            Prop: prop,
+            Time: time,
+        }
+    }
+    return nodes, nil
+}
+
+// NebulaReader uses nebula-go sdk to connect to nebula-graphd service
+type NebulaReadWriter struct {
+    address         string  // address of nebula-graphd
+    port            int     // port of nebula-graphd
+    username        string
+    password        string
+    graphName       string  // name of graph(or space), all the operations are applied to this graph
+    pool            *nebula.ConnectionPool
+}
+
+func NewNebulaReadWriter(address string, port int,
+        username, password, graphName string) (*NebulaReadWriter, error) {
     log := nebula.DefaultLogger{}
     hostAddress := nebula.HostAddress{Host: address, Port: port}
     hostList := []nebula.HostAddress{hostAddress}
@@ -32,7 +58,7 @@ func NewNebulaReader(address string, port int,
             address, port, err))
     }
 
-    return &NebulaReader{
+    return &NebulaReadWriter{
         address: address,
         port: port,
         username: username,
@@ -42,8 +68,53 @@ func NewNebulaReader(address string, port int,
     }, nil
 }
 
-func (s *NebulaReader) Close() {
+func (s *NebulaReadWriter) Close() {
     s.pool.Close()
+}
+
+func (s *NebulaReadWriter) LookupWithTimeLimit(node *PrincipleNode, timeRange [2]time.Time) ([]string, error) {
+    startTime := fmt.Sprintf("datetime(\"%s\")", timeRange[0].Format("2006-01-02T15:04:05.000000"))
+    endTime := fmt.Sprintf("datetime(\"%s\")", timeRange[1].Format("2006-01-02T15:04:05.000000"))
+
+    timeProp := fmt.Sprintf("%s.%s", node.Name, node.Time)
+    idProp := fmt.Sprintf("%s.%s", node.Name, node.Prop)
+
+    query := fmt.Sprintf("USE %s; LOOKUP ON %s WHERE %s > %s and %s < %s YIELD %s;",
+        s.graphName, node.Name, timeProp, startTime, timeProp, endTime, idProp)
+
+    resultSet, err := s.Query(query)
+
+    if err != nil {
+        return []string{}, err
+    }
+
+    colNames := resultSet.GetColNames()
+    rowNum := resultSet.GetRowSize()
+    log.WithFields(log.Fields{
+        "col_names": colNames,
+        "num_row": rowNum,
+        "result": resultSet,
+    }).Debug("Got results")
+
+    unwrappedData := make([]string, rowNum)
+    dataList, err := resultSet.GetValuesByColName(idProp)
+    if err != nil {
+        return nil, errors.New(fmt.Sprintf("Reading data of col %s failed, err: %s\n", idProp, err))
+    }
+
+    for i, data := range dataList {
+        unwrappedData[i], err = data.AsString()
+        if err != nil {
+            log.WithFields(log.Fields{
+                "col": idProp,
+                "row": i,
+                "error": err,
+            }).Warn("Reading data failed, skip")
+            continue
+        }
+    }
+
+    return unwrappedData, nil
 }
 
 /*
@@ -61,7 +132,7 @@ Return:
        {'email': ['abc@gmail'], 'telephone': ['13133445432'],
        'province': ['Shandong', 'Beijing']}
 */
-func (s *NebulaReader) GetMultiNeighbors(name string, edgeProp map[string]string) (map[string][]string, error) {
+func (s *NebulaReadWriter) GetMultiNeighbors(name string, edgeProp map[string]string) (map[string][]string, error) {
     propList := make([]string, 0)
     queryList := make([]string, 0)
     for edge, prop := range edgeProp {
@@ -80,18 +151,25 @@ func (s *NebulaReader) GetMultiNeighbors(name string, edgeProp map[string]string
     for i, resultSet := range resultSetList {
         result, err := resultSet.GetValuesByColName(propList[i])
         if err != nil {
-            fmt.Printf("Unwrap the result of query '%s' failed, err: %s", queryList[i], err)
+            log.WithFields(log.Fields{
+                "query": queryList[i],
+                "error": err,
+            }).Warn("Unwraping the result of query failed")
             continue
         }
         if result == nil {
-            fmt.Printf("The result of query '%s' is empty, skip", queryList[i])
+            log.WithField("query", queryList[i]).Warn("The result of query is empty")
             continue
         }
         unwrappedDataDict[propList[i]] = make([]string, 0)
         for j, data := range result {
             unwrappedData, err := data.AsString()
             if err != nil {
-                fmt.Printf("Unwrap data of (col %s, row %d) failed, err: %s, skip", propList[i], j, err)
+                log.WithFields(log.Fields{
+                    "col": propList[i],
+                    "row": j,
+                    "error": err,
+                }).Warn("Unwraping data failed")
             }
             unwrappedDataDict[propList[i]] = append(unwrappedDataDict[propList[i]], unwrappedData)
         }
@@ -109,7 +187,7 @@ Params:
 
 Return:
 */
-func (s *NebulaReader) GetSingleNeighbor(name, edge, prop string) ([]string, error) {
+func (s *NebulaReadWriter) GetSingleNeighbor(name, edge, prop string) ([]string, error) {
     query := fmt.Sprintf(
                 "USE %s; GO FROM hash(\"%s\") OVER %s YIELD properties($$).%s AS %s;",
                 s.graphName, name, edge, prop, prop)
@@ -121,7 +199,10 @@ func (s *NebulaReader) GetSingleNeighbor(name, edge, prop string) ([]string, err
 
     colNames := result.GetColNames()
     rowNum := result.GetRowSize()
-    fmt.Printf("col name: %+v, num of row: %d\n", colNames, rowNum)
+    log.WithFields(log.Fields{
+        "col_names": colNames,
+        "row_num": rowNum,
+    }).Debug()
 
     unwrappedData := make([]string, rowNum)
     dataList, err := result.GetValuesByColName(prop)
@@ -132,7 +213,11 @@ func (s *NebulaReader) GetSingleNeighbor(name, edge, prop string) ([]string, err
     for i, data := range dataList {
         unwrappedData[i], err = data.AsString()
         if err != nil {
-            fmt.Printf("Read data of (col %s, row %d) failed, err: %s, skip\n", prop, i, err)
+            log.WithFields(log.Fields{
+                "col": prop,
+                "row": i,
+                "error": err,
+            }).Warn("Reading data failed, skip")
             continue
         }
     }
@@ -141,7 +226,7 @@ func (s *NebulaReader) GetSingleNeighbor(name, edge, prop string) ([]string, err
 }
 
 // MultiQuery executes a list of queries sequentially and return a list of ResultSets
-func (s *NebulaReader) MultiQuery(queryList []string) ([]*nebula.ResultSet, error) {
+func (s *NebulaReadWriter) MultiQuery(queryList []string) ([]*nebula.ResultSet, error) {
     session, err := s.pool.GetSession(s.username, s.password)
     if err != nil {
         return nil, err
@@ -152,12 +237,18 @@ func (s *NebulaReader) MultiQuery(queryList []string) ([]*nebula.ResultSet, erro
     for i, query := range queryList {
         result, err := session.Execute(query)
         if err != nil {
-            fmt.Printf("Failed to execute query '%s', err: %s, skip", query, err)
+            log.WithFields(log.Fields{
+                "query": query,
+                "error": err,
+            }).Warn("Failed to execute query, skip")
             resultSets[i] = nil
             continue
         }
         if err := checkResultSet(query, result); err != nil {
-            fmt.Printf("Failed to check the result of query '%s', err: %s, skip", query, err)
+            log.WithFields(log.Fields{
+                "query": query,
+                "error": err,
+            }).Warn("Failed to check the result of query")
             resultSets[i] = nil
             continue
         }
@@ -167,7 +258,7 @@ func (s *NebulaReader) MultiQuery(queryList []string) ([]*nebula.ResultSet, erro
 }
 
 // Query executes a single query and return the ResultSet
-func (s *NebulaReader) Query(query string) (*nebula.ResultSet, error) {
+func (s *NebulaReadWriter) Query(query string) (*nebula.ResultSet, error) {
     session, err := s.pool.GetSession(s.username, s.password)
     if err != nil {
         return nil, err
--- pkg/graph/nebula_graph_test.go
+++ pkg/graph/nebula_graph_test.go
@@ -1,7 +1,10 @@
 package graph
 
 import (
+	"fmt"
 	"testing"
+	"time"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,16 +13,12 @@ func TestQuery(t *testing.T) {
 					   "CDWc@gmail.com", "eDJWvJZwUk@gmail.com",
 					   "ieBaArcxUR@gmail.com", "uLi@gmail.com"}
 
-	nebula, err := NewNebulaReader("192.168.31.147", 9669, "root", "nebula", "relation_graph")
-	if err != nil {
-		t.Fatal(err)
-	}
+	nebula, err := NewNebulaReadWriter("192.168.31.147", 9669, "root", "nebula", "relation_graph")
+	assert.NoError(t, err)
 	defer nebula.Close()
 
 	data, err := nebula.GetSingleNeighbor("121904329086390421", "id_email", "email")
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	t.Logf("data: %+v", data)
 	
 	assert.Equal(t, target, data)
@@ -37,10 +36,8 @@ func TestMultiQuery(t *testing.T) {
 	target["telephone"] = []string{"10927866150", "14326585168", "18595097300",
 								   "11660492056", "13992190315"}
 
-	nebula, err := NewNebulaReader("192.168.31.147", 9669, "root", "nebula", "relation_graph")
-	if err != nil {
-		t.Fatal(err)
-	}
+	nebula, err := NewNebulaReadWriter("192.168.31.147", 9669, "root", "nebula", "relation_graph")
+	assert.NoError(t, err)
 	defer nebula.Close()
 
 	edgeProp := map[string]string{
@@ -50,10 +47,31 @@ func TestMultiQuery(t *testing.T) {
 	}
 
 	data, err := nebula.GetMultiNeighbors("121904329086390421", edgeProp)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	t.Logf("data: %+v", data)
 
 	assert.Equal(t, target, data)
+}
+
+func TestLookup(t *testing.T) {
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	assert.NoError(t, err)
+
+	startTime := time.Date(2021, 12, 17, 15, 41, 0, 0, loc)
+	endTime := time.Date(2021, 12, 17, 15, 42, 0, 0, loc)
+	timeRange := [2]time.Time{startTime, endTime}
+
+	node := PrincipleNode{
+		Name: "id",
+		Prop: "id",
+		Time: "collect_time",	
+	}
+
+	nebula, err := NewNebulaReadWriter("192.168.31.147", 9669, "root", "nebula", "relation_graph")
+	assert.NoError(t, err)
+	defer nebula.Close()
+
+	data, err := nebula.LookupWithTimeLimit(&node, timeRange)
+	assert.NoError(t, err)
+	fmt.Printf("data: %+v\n", data)
 }
